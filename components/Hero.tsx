5fc8633bd2787571a094161d4bccfcb4ea7587f8
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-02-14 16:47:06 +0100

    zmiana opisu w naglowku

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-
 const Hero = () => {
 	return (
 		<div>
@@ -23,17 +22,18 @@ const Hero = () => {
 			<div className='flex justify-center relative my-20 z-10'>
 				<div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
 					<h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-						Chwytliwe hasełko nr1
+						Scalable, Fast & Beautiful
 					</h2>
 
 					<TextGenerateEffect
 						className='text-center text-[40px] md:text-5xl lg:text-6xl'
 						duration={2}
-						words='Animowane hasełko i kolejne SŁOWO jest purple'
+						words='Crafting Web Solutions with React, TypeScript & a Dash of Azure'
 					/>
 
-					<p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-						Cześć! jestem Bartek i jestem super
+					<p className='text-center md:tracking-wider m-4 text-sm md:text-lg lg:text-2xl'>
+						Hi, I&apos;m Bartlomiej, a Junior FullStack Developer based in
+						Krakow.
 					</p>
 				</div>
 			</div>

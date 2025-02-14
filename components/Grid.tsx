88938fd294d81f89b7fa85653f14755d73f68dc6
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-02-14 19:49:33 +0100

    grid cd..

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,16 +6,30 @@ const Grid = () => {
 	return (
 		<section id='about'>
 			<BentoGrid>
-				{gridItems.map(({id, title, description, className, img}) => (
-					<BentoGridItem
-						id={id}
-						key={id}
-						title={title}
-						description={description}
-						className={className}
-						img={img}
-					/>
-				))}
+				{gridItems.map(
+					({
+						id,
+						title,
+						description,
+						className,
+						img,
+						imgClassName,
+						titleClassName,
+						spareImg,
+					}) => (
+						<BentoGridItem
+							id={id}
+							key={id}
+							title={title}
+							description={description}
+							className={className}
+							img={img}
+							imgClassName={imgClassName}
+							titleClassName={titleClassName}
+							spareImg={spareImg}
+						/>
+					)
+				)}
 			</BentoGrid>
 		</section>
 	);

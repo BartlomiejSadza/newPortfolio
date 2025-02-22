7fe3b83fe84b7e121d765c1a295540d58a49ba62
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-02-22 14:32:43 +0100

    slight text changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -113,7 +113,7 @@ export const BentoGridItem = ({
 							</div>
 							<div className='flex flex-col gap-3 lg:gap-8'>
 								<span className='py-4 px-3 rounded-lg text-center bg-[#10132e]' />
-								{["Azure", "Python", "React Native"].map((item, i) => (
+								{["Azure DevOps", "Python", "Figma Design"].map((item, i) => (
 									<span
 										key={i}
 										className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]'>

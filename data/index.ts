5a82c4f9e6cd014764a163e555c67fd49e6bb91b
Bartłomiej Sadza <bartlomiejsadza@Bartomiejs-MacBook-Air.local>
2025-10-18 23:16:08 +0200

    testowy

--- data/index.ts
+++ data/index.ts
@@ -52,7 +52,7 @@ export const gridItems = [
 	{
 		id: 5,
 		title:
-			"Currently exploring the magic of SSR rendering technology & Expo.js",
+			"Currently exploring the magic of SSR rendering technology & Expo.js chyba xD",
 		description: "The Inside Scoop",
 		className: "md:col-span-3 md:row-span-2",
 		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
